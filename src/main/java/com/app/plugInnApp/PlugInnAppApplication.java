3822b6523ecf456d386a215f2f0c5e749563b2af
Harsh Pandey <hars30896@gmail.com>
2024-01-23 23:40:37 +0530

    first commit

---
+++ src/main/java/com/app/plugInnApp/PlugInnAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.plugInnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugInnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugInnAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/plugInnApp/PlugInnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.plugInnApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugInnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
